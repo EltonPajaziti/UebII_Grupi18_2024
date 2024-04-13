072e55aa3d1264f6cdb4fba5ab8990237aec9903
EltonPajaziti <121615151+EltonPajaziti@users.noreply.github.com>
2024-04-14 00:33:33 +0200

    add hometable.php

---
+++ hometable.php
@@ -0,0 +1,48 @@
+<div id="time-table" class="time-table-section">
+         <div class="container">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+               <div class="row">
+                  <div class="service-time one" style="background:#2895f1;">
+                     <span class="info-icon"><i class="fa fa-ambulance" aria-hidden="true"></i></span>
+                     <h3>Emergency Case</h3>
+                     <p>Dignissimos ducimus qui blanditii sentium volta tum deleniti atque cori as quos dolores et quas mole.</p>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+               <div class="row">
+                  <div class="service-time middle" style="background:#0071d1;">
+                     <span class="info-icon"><i class="fa fa-clock-o" aria-hidden="true"></i></span> 
+                     <h3>Working Hours</h3>
+                     <div class="time-table-section">
+                        <ul>
+                 
+                           <?php
+                            // Deklarimi i vargut me oraret e punës
+                            //Perdorimi i vargut asociativ 
+                            $workingHours = [
+                                "Monday - Friday" => "8.00 – 18.00",
+                                "Saturday" => "8.00 – 16.00",
+                                "Sunday" => "8.00 – 13.00"
+                            ];
+
+                            // Shfaqja e orareve të punës nga vargu
+                            foreach($workingHours as $days => $hours): ?>
+                                <li><span class="left"><?php echo $days; ?></span><span class="right"><?php echo $hours; ?></span></li>
+                            <?php endforeach; ?>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+               <div class="row">
+                  <div class="service-time three" style="background:#0060b1;">
+                     <span class="info-icon"><i class="fa fa-hospital-o" aria-hidden="true"></i></span>
+                     <h3>Clinic Timetable</h3>
+                     <p>Dignissimos ducimus qui blanditii sentium volta tum deleniti atque cori as quos dolores et quas mole.</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
--- index.php
+++ index.php
@@ -38,44 +38,9 @@
       <!-- END LOADER -->
       <?php include 'header.php'; ?>
       <?php include 'home.php'; ?>
+      <?php include 'hometable.php'; ?>
       <!-- end section -->
-      <div id="time-table" class="time-table-section">
-         <div class="container">
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-               <div class="row">
-                  <div class="service-time one" style="background:#2895f1;">
-                     <span class="info-icon"><i class="fa fa-ambulance" aria-hidden="true"></i></span>
-                     <h3>Emergency Case</h3>
-                     <p>Dignissimos ducimus qui blanditii sentium volta tum deleniti atque cori as quos dolores et quas mole.</p>
-                  </div>
-               </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-               <div class="row">
-                  <div class="service-time middle" style="background:#0071d1;">
-                     <span class="info-icon"><i class="fa fa-clock-o" aria-hidden="true"></i></span> 
-                     <h3>Working Hours</h3>
-                     <div class="time-table-section">
-                        <ul>
-                           <li><span class="left">Monday - Friday</span><span class="right">8.00 – 18.00</span></li>
-                           <li><span class="left">Saturday</span><span class="right">8.00 – 16.00</span></li>
-                           <li><span class="left">Sunday</span><span class="right">8.00 – 13.00</span></li>
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-               <div class="row">
-                  <div class="service-time three" style="background:#0060b1;">
-                     <span class="info-icon"><i class="fa fa-hospital-o" aria-hidden="true"></i></span>
-                     <h3>Clinic Timetable</h3>
-                     <p>Dignissimos ducimus qui blanditii sentium volta tum deleniti atque cori as quos dolores et quas mole.</p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
+    
       <div id="about" class="section wow fadeIn">
          <div class="container">
             <div class="heading">
