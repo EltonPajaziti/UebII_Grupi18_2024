e1a58b067cb2d1e9291b7918d8a1a4a58625634c
EltonPajaziti <121615151+EltonPajaziti@users.noreply.github.com>
2024-04-14 00:06:35 +0200

    shtim ne header.php

--- php/index.php
+++ php/index.php
@@ -12,86 +12,32 @@
    <meta name="description" content="">
    <meta name="author" content="">
    <!-- Site Icons -->
-   <link rel="shortcut icon" href="images/fevicon.ico.png" type="image/x-icon" />
+   <link rel="shortcut icon" href="../images/fevicon.ico.png" type="image/x-icon" />
    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
    <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="../css/bootstrap.min.css">
    <!-- Site CSS -->
    <link rel="stylesheet" href="style.css">
    <!-- Colors CSS -->
-   <link rel="stylesheet" href="css/colors.css">
+   <link rel="stylesheet" href="../css/colors.css">
    <!-- ALL VERSION CSS -->
-   <link rel="stylesheet" href="css/versions.css">
+   <link rel="stylesheet" href="../css/versions.css">
    <!-- Responsive CSS -->
-   <link rel="stylesheet" href="css/responsive.css">
+   <link rel="stylesheet" href="../css/responsive.css">
    <!-- Custom CSS -->
-   <link rel="stylesheet" href="css/custom.css">
+   <link rel="stylesheet" href="../css/custom.css">
    <!-- Modernizer for Portfolio -->
-   <script src="js/modernizer.js"></script>
+   <script src="../js/modernizer.js"></script>
    <!-- [if lt IE 9] -->
    </head>
    <body class="clinic_version">
       <!-- LOADER -->
       <div id="preloader">
-         <img class="preloader" src="images/loaders/heart-loading2.gif" alt="">
+         <img class="preloader" src="../images/loaders/heart-loading2.gif" alt="">
       </div>
       <!-- END LOADER -->
-      <header>
-         <div class="header-top wow fadeIn">
-            <div class="container">
-               <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="image"></a>
-               <div class="right-header">
-                  <div class="header-info">
-                     <div class="info-inner">
-                        <span class="icontop"><img src="images/phone-icon.png" alt="#"></span>
-                        <span class="iconcont"><a href="tel:800 123 456">800 123 456</a></span>	
-                     </div>
-                     <div class="info-inner">
-                        <span class="icontop"><i class="fa fa-envelope" aria-hidden="true"></i></span>
-                        <span class="iconcont"><a data-scroll href="mailto:info@yoursite.com">info@Lifecare.com</a></span>	
-                     </div>
-                     <div class="info-inner">
-                        <span class="icontop"><i class="fa fa-clock-o" aria-hidden="true"></i></span>
-                        <span class="iconcont"><a data-scroll href="#">Daily: 7:00am - 8:00pm</a></span>	
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div class="header-bottom wow fadeIn">
-            <div class="container">
-               <nav class="main-menu">
-                  <div class="navbar-header">
-                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i class="fa fa-bars" aria-hidden="true"></i></button>
-                  </div>
-				  
-                  <div id="navbar" class="navbar-collapse collapse">
-                     <ul class="nav navbar-nav">
-                        <li><a class="active" href="index.html">Home</a></li>
-                        <li><a data-scroll href="#about">About us</a></li>
-                        <li><a data-scroll href="#service">Services</a></li>
-                        <li><a data-scroll href="#doctors">Doctors</a></li>
-                        <li><a data-scroll href="#price">Price</a></li>
-						<li><a data-scroll href="#testimonials">Testimonials</a></li>
-                        <li><a data-scroll href="#getintouch">Contact</a></li>
-                     </ul>
-                  </div>
-               </nav>
-               <div class="serch-bar">
-                  <div id="custom-search-input">
-                     <div class="input-group col-md-12">
-                        <input type="text" class="form-control input-lg" placeholder="Search" />
-                        <span class="input-group-btn">
-                        <button class="btn btn-info btn-lg" type="button">
-                        <i class="fa fa-search" aria-hidden="true"></i>
-                        </button>
-                        </span>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </header>
+      <?php include 'header.php'; ?>
+      
       <div id="home" class="parallax first-section wow fadeIn" data-stellar-background-ratio="0.4" style="background-image:url('images/slider-bg.png');">
          <div class="container">
             <div class="row">
