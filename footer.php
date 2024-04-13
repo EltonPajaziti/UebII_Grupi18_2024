93aa35d1b02d7521a78589743c868333dafbe0d4
EltonPajaziti <121615151+EltonPajaziti@users.noreply.github.com>
2024-04-14 01:35:04 +0200

    added footer.php

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer id="footer" class="footer-area wow fadeIn">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="logo padding">
+                     <a href=""><img src="images/logo.png" alt=""></a>
+                     <p>Locavore pork belly scen ester pine est chill wave microdosing pop uple itarian cliche artisan.</p>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="footer-info padding">
+                     <h3>CONTACT US</h3>
+                     <p><i class="fa fa-map-marker" aria-hidden="true"></i> PO Box 16122 Collins Street West Victoria 8007 Australia</p>
+                     <p><i class="fa fa-paper-plane" aria-hidden="true"></i> info@gmail.com</p>
+                     <p><i class="fa fa-phone" aria-hidden="true"></i> (+1) 800 123 456</p>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="subcriber-info">
+                     <h3>SUBSCRIBE</h3>
+                     <p>Get healthy news, tip and solutions to your problems from our experts.</p>
+                     <div class="subcriber-box">
+                        <form id="mc-form" class="mc-form">
+                           <div class="newsletter-form">
+                              <input type="email" autocomplete="off" id="mc-email" placeholder="Email address" class="form-control" name="EMAIL">
+                              <button class="mc-submit" type="submit"><i class="fa fa-paper-plane"></i></button> 
+                              <div class="clearfix"></div>
+                              <!-- mailchimp-alerts Start -->
+                              <div class="mailchimp-alerts">
+                                 <div class="mailchimp-submitting"></div>
+                                 <!-- mailchimp-submitting end -->
+                                 <div class="mailchimp-success"></div>
+                                 <!-- mailchimp-success end -->
+                                 <div class="mailchimp-error"></div>
+                                 <!-- mailchimp-error end -->
+                              </div>
+                              <!-- mailchimp-alerts end -->
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
--- index.php
+++ index.php
@@ -46,11 +46,9 @@
       <?php include 'statistikat2.php'; ?>
       <?php include 'keshillat.php'; ?>
       <?php include 'getintouch.php'; ?>
-
-
-
       <?php include 'price.php'; ?>
       <?php include 'testimonials.php'; ?>
+      <?php include 'footer.php'; ?>
 
       <!-- end section -->
     
@@ -67,96 +65,8 @@
 	  
       
       <!-- end section -->
-      <div id="getintouch" class="section wb wow fadeIn" style="padding-bottom:0;">
-         <div class="container">
-            <div class="heading">
-               <span class="icon-logo"><img src="images/icon-logo.png" alt="#"></span>
-               <h2>Get in Touch</h2>
-            </div>
-         </div>
-         <div class="contact-section">
-            <div class="form-contant">
-               <form id="ajax-contact" action="assets/mailer.php" method="post">
-                  <div class="row">
-                     <div class="col-md-6">
-                        <div class="form-group in_name">
-                           <input type="text" class="form-control" placeholder="Name" required="required">
-                        </div>
-                     </div>
-                     <div class="col-md-6">
-                        <div class="form-group in_email">
-                           <input type="email" class="form-control" placeholder="E-mail" required="required">
-                        </div>
-                     </div>
-                     <div class="col-md-6">
-                        <div class="form-group in_email">
-                           <input type="tel" class="form-control" id="phone" placeholder="Phone" required="required">
-                        </div>
-                     </div>
-                     <div class="col-md-6">
-                        <div class="form-group in_email">
-                           <input type="text" class="form-control" id="subject" placeholder="Subject" required="required">
-                        </div>
-                     </div>
-                     <div class="col-md-12">
-                        <div class="form-group in_message"> 
-                           <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required"></textarea>
-                        </div>
-                        <div class="actions">
-                           <input type="submit" value="Send Message" name="submit" id="submitButton" class="btn small" title="Submit Your Message!">
-                        </div>
-                     </div>
-                  </div>
-               </form>
-            </div>
-            <div id="googleMap" style="width:100%;height:450px;"></div>
-         </div>
-      </div>
-      <footer id="footer" class="footer-area wow fadeIn">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-4">
-                  <div class="logo padding">
-                     <a href=""><img src="images/logo.png" alt=""></a>
-                     <p>Locavore pork belly scen ester pine est chill wave microdosing pop uple itarian cliche artisan.</p>
-                  </div>
-               </div>
-               <div class="col-md-4">
-                  <div class="footer-info padding">
-                     <h3>CONTACT US</h3>
-                     <p><i class="fa fa-map-marker" aria-hidden="true"></i> PO Box 16122 Collins Street West Victoria 8007 Australia</p>
-                     <p><i class="fa fa-paper-plane" aria-hidden="true"></i> info@gmail.com</p>
-                     <p><i class="fa fa-phone" aria-hidden="true"></i> (+1) 800 123 456</p>
-                  </div>
-               </div>
-               <div class="col-md-4">
-                  <div class="subcriber-info">
-                     <h3>SUBSCRIBE</h3>
-                     <p>Get healthy news, tip and solutions to your problems from our experts.</p>
-                     <div class="subcriber-box">
-                        <form id="mc-form" class="mc-form">
-                           <div class="newsletter-form">
-                              <input type="email" autocomplete="off" id="mc-email" placeholder="Email address" class="form-control" name="EMAIL">
-                              <button class="mc-submit" type="submit"><i class="fa fa-paper-plane"></i></button> 
-                              <div class="clearfix"></div>
-                              <!-- mailchimp-alerts Start -->
-                              <div class="mailchimp-alerts">
-                                 <div class="mailchimp-submitting"></div>
-                                 <!-- mailchimp-submitting end -->
-                                 <div class="mailchimp-success"></div>
-                                 <!-- mailchimp-success end -->
-                                 <div class="mailchimp-error"></div>
-                                 <!-- mailchimp-error end -->
-                              </div>
-                              <!-- mailchimp-alerts end -->
-                           </div>
-                        </form>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </footer>
+
+    
       <div class="copyright-area wow fadeIn">
          <div class="container">
             <div class="row">
